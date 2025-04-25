dc0228f43ca65d1ae7616dd0138657e6b9bcda4c
MW116400 <MW116400@MGRW42NLPC2330>
2025-04-25 10:14:27 +0530

    correct the test coverage

--- UnitTestProject1/M_UNIT_UnitTest1.cs
+++ UnitTestProject1/M_UNIT_UnitTest1.cs
@@ -13,18 +13,24 @@ namespace UnitTestProject1
         public void TestSum()
         {
             Assert.AreEqual(30, Arithmetic.Sum(i, j));
+            Assert.AreEqual(0, Arithmetic.Sum(0, 0));
+            Assert.AreEqual(-10, Arithmetic.Sum(-5, -5));
         }
 
         [TestMethod]
         public void TestSub()
         {
             Assert.AreEqual(-10, Arithmetic.Sub(i, j));
+            Assert.AreEqual(0, Arithmetic.Sub(5, 5));
+            Assert.AreEqual(10, Arithmetic.Sub(5, -5));
         }
 
         [TestMethod]
         public void TestMul()
         {
             Assert.AreEqual(200, Arithmetic.Mul(i, j));
+            Assert.AreEqual(0, Arithmetic.Mul(0, 5));
+            Assert.AreEqual(-25, Arithmetic.Mul(5, -5));
         }
 
         [TestMethod]
@@ -32,6 +38,7 @@ namespace UnitTestProject1
         {
             Assert.AreEqual(2, Arithmetic.Div(20, 10));
             Assert.ThrowsException<DivideByZeroException>(() => Arithmetic.Div(20, 0));
+            Assert.AreEqual(-4, Arithmetic.Div(20, -5));
         }
     }
 }
